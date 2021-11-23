ab46c372946350b342cd36309264be18be577cd1
esau91 <esau.gk.91@gmail.com>
2021-11-22 21:57:45 -0600

    Finish script and testing

--- scripts/data_processing.py
+++ scripts/data_processing.py
@@ -3,18 +3,64 @@
 #Date: 19/11/2021
 #Description: 
 
+'''
+    This program will do the following:
+
+    1. Read by batches a JSON file with multiple JSON objects with the following structure:
+        {"event_type": 7, "event_time": "2019-01-04 05:07:26", "data": {"user_email": "TAFgZ@gmail.com", "phone_number": "0603728764"}, "processing_date": "2019-01-04"}
+    2. Apply the following validations to each JSON object:
+        event_type -> int
+        event_time -> datetime
+        user_email -> string
+        phone_number ->  string
+        processing_date -> date
+       If a object doesn't pass the validations the row will be discarted and written into an error file to be validated later.
+    3. Write all the valid rows to a database with the following schema:
+        | event_type | event_time | user_email | phone_number | processing_date | file_name |
+    
+    The variables in the main function that you need to configure:
+
+        file_path = name of the input JSON file
+        error_file_path = name of the error file to be created
+        db_connection_params = {
+            'my_host': , 
+            'my_port': , 
+            'my_db': , 
+            'my_charset': 'utf8mb4',
+            'my_user': , 
+            'my_password': 
+        }
+        batch_size = define the size of the batch to process data
+
+'''
+
 import io
-import json
 import time
 import ijson
 import pymysql
 import logging
 from datetime import datetime, date
 
-# One record from input file
-# {"event_type": 7, "event_time": "2019-01-04 05:07:26", "data": {"user_email": "TAFgZ@gmail.com", "phone_number": "0603728764"}, "processing_date": "2019-01-04"}
+total_counter_processed = 0
+total_counter_written = 0
+total_error_found = 0
+
+def datetime_valid(dt_str):
+    try:
+        datetime.fromisoformat(dt_str)
+    except:
+        return False
+    return True
+
+def date_valid(dt_str):
+    try:
+        date.fromisoformat(dt_str)
+    except:
+        return False
+    return True
 
 def process_data(data, file_name, field_names, error_file_path):    
+    global total_counter_processed, total_error_found
     processed_data = []
     error_data = []
     error_counter = 0
@@ -23,36 +69,42 @@ def process_data(data, file_name, field_names, error_file_path):
     for row in data:
         values = []
         if  isinstance(row[field_names[0]], int) and \
-            isinstance(datetime.fromisoformat(row[field_names[1]]), datetime) and \
-            isinstance(row['data'][field_names[2]], str) and \
-            isinstance(row['data'][field_names[3]], str) and \
-            isinstance(date.fromisoformat(row[field_names[4]]), date):
+            datetime_valid(row[field_names[1]]) and \
+            isinstance(row[field_names[2]], str) and \
+            isinstance(row[field_names[3]], str) and \
+            date_valid(row[field_names[4]]):
+             #isinstance(datetime.fromisoformat(row[field_names[1]]), datetime) and \        
+            #isinstance(date.fromisoformat(row[field_names[4]]), date):
 
             success_counter += 1
+            total_counter_processed += 1
 
             values.append(row[field_names[0]])
             values.append(row[field_names[1]])
-            values.append(row['data'][field_names[2]])
-            values.append(row['data'][field_names[3]])
+            values.append(row[field_names[2]])
+            values.append(row[field_names[3]])
             values.append(row[field_names[4]])
             values.append(file_name)        
 
             processed_data.append(values)
         else:
+            total_error_found += 1
             error_counter += 1
             error_data.append(row)
     
-    if len(error_data) > 1:
+    if len(error_data) > 0:
         my_date = datetime.now().date()
-        error_file = error_file_path + my_date + '.json'
+        error_file = error_file_path + str(my_date) + '.json'
 
         with open(error_file, 'w') as f:
             for item in error_data:
                 f.write("%s\n" % item)
 
-        print('\n{} datatypes error were found, check out the file: {}'.format(error_counter, error_file))
+        print('Data Types Errors: {}'.format(error_counter))
+        logging.info('Data Types Errors: {}'.format(error_counter))
     
-    print('\nProcessed records: {}'.format(success_counter))
+    print('Processed records: {}'.format(success_counter))
+    logging.info('Processed records: {}'.format(success_counter))
 
     return processed_data, success_counter
             
@@ -71,12 +123,14 @@ def connect_to_db(db_connection_params):
         cursor = connection.cursor()
     except Exception as e:
         print('The following exception was thrown while trying to connect to the DB: {}'.format(e))
+        logging.info('The following exception was thrown while trying to connect to the DB: {}'.format(e))
         print(db_connection_params)
         exit(1)
 
     return connection, cursor
 
 def write_to_db(db_connection_params, processed_data, success_counter):
+    global total_counter_written
     connection, cursor = connect_to_db(db_connection_params)
     query = "INSERT INTO user_events VALUES(%s, %s, %s, %s, %s, %s)"
     
@@ -84,43 +138,83 @@ def write_to_db(db_connection_params, processed_data, success_counter):
         response = cursor.executemany(query, processed_data)
         connection.commit()
         cursor.close()
+        total_counter_written += response
         if success_counter == response:
-            print('\nWritten records: {}'.format(response))
+            print('Written records: {}'.format(response))
+            logging.info('Written records: {}'.format(response))
         else:
-            print('\nThere were {} records that culd not be written to the DB'.format(processed_data - response))
+            print('There were {} records that culd not be written to the DB'.format(processed_data - response))
+            logging.info('There were {} records that culd not be written to the DB'.format(processed_data - response))
     except Exception as e:
         print('The following exception was thrown while trying to write to DB: {}'.format(e))        
+        logging.info('The following exception was thrown while trying to write to DB: {}'.format(e))
         exit(1)
 
-def read_json(file_path):
-    data = []
-    for line in open(file_path, 'r'):
-        data.append(json.loads(line))
+def process_and_write(line_number, start_time, data, file_name, field_names, error_file_path, db_connection_params):
+    print('Line: {}'.format(line_number + 1))
+    logging.info('Line: {}'.format(line_number + 1))
+    processed_data, success_counter = process_data(data, file_name, field_names, error_file_path)
+    process_time = time.time()
+    logging.info('Time: {0:.2f}'.format(process_time - start_time))
+    print('Time: {0:.2f} seconds'.format(process_time - start_time))
+    write_to_db(db_connection_params, processed_data, success_counter)
+    logging.info('Time: {0:.2f}'.format(time.time() - process_time))
+    print('Time: {0:.2f} seconds'.format(time.time() - process_time))
+    print('\n#######################################################')
+    return 0
 
-    return data
-
-def partial_json_reading(file_path):
+def partial_json_processing(file_path, file_name, batch_size, field_names, error_file_path, db_connection_params):
     data = []
+    start_time = time.time()
 
     with open(file_path) as json_file:
-        cursor = 0
+        row_counter = 0
         for line_number, line in enumerate(json_file):
-            print('Line: {}, Cursor: {}'.format(line_number+1, cursor))
             line_as_file = io.StringIO(line)
             json_parser = ijson.parse(line_as_file)
 
             for prefix, type, value in json_parser:
-                if prefix in ['event_type', 'event_time', 'data.user_email', 'data.phone_number', 'processing_date']:
-                    print('Prefix: {}, Type: {}, Value: {}'.format(prefix, type, value))
-            cursor += len(line)
+                if prefix == 'event_type':
+                    event_type = value
+                elif prefix == 'event_time':
+                    event_time = value
+                elif prefix == 'data.user_email':
+                    user_mail = value
+                elif prefix == 'data.phone_number':
+                    phone_number = value
+                elif prefix == 'processing_date':
+                    processing_date = value
 
-        return data
+            json_values = {
+                'event_type': event_type, 
+                'event_time': event_time, 
+                'user_email': user_mail, 
+                'phone_number': phone_number, 
+                'processing_date': processing_date, 
+                'file_name': file_name 
+            }
+            row_counter += 1
+
+            data.append(json_values)
+
+            if row_counter == batch_size:                
+                process_and_write(line_number, start_time, data, file_name, field_names, error_file_path, db_connection_params)
+                data = []
+                row_counter = 0
+                start_time = time.time()
+        
+    if len(data) > 0:        
+        process_and_write(line_number, start_time, data, file_name, field_names, error_file_path, db_connection_params)
+        data = []
+        row_counter = 0
+        start_time = time.time()
+    
+    return 0
 
 def main():
+    ############################################ Variables ############################################
     file_path = '/Users/esau/Downloads/Flink/202106_flink_data_engieering_sample_data.json'
-    error_file_path = '/Users/esau/Downloads/Flink/error_'
-    field_names = ['event_type', 'event_time', 'user_email', 'phone_number', 'processing_date', 'file_name']
-    file_name = file_path.split('/')[-1]
+    error_file_path = '/Users/esau/Git/Flink/scripts/error_'
     db_connection_params = {
         'my_host': 'localhost', 
         'my_port': 55545, 
@@ -129,26 +223,26 @@ def main():
         'my_user': 'root', 
         'my_password': 'root'
     }
-    
+    batch_size = 2500
+    #####################################################################################################
+    field_names = ['event_type', 'event_time', 'user_email', 'phone_number', 'processing_date', 'file_name']
+    file_name = file_path.split('/')[-1]
+    start_time = time.time()
+
     logging.basicConfig(filename='my_loggging_info.log', encoding='utf-8', format='%(asctime)s %(message)s', datefmt='%Y/%m/%d %I:%M:%S %p', level=logging.INFO)
     logging.info('Data Processing Script Starts')
 
-    start_time = time.time()
-    data = read_json(file_path)
-    processed_data, success_counter = process_data(data, file_name, field_names, error_file_path)
-    process_time = time.time()
+    partial_json_processing(file_path, file_name, batch_size, field_names, error_file_path, db_connection_params)
 
-    logging.info('Data Processing Time: {0:.2f}'.format(process_time - start_time))
-    print('\nTime: {0:.2f} seconds'.format(process_time - start_time))
-
-    write_to_db(db_connection_params, processed_data, success_counter)
-
-    logging.info('Data Writing Time: {0:.2f}'.format(time.time() - process_time))
-    print('\nTime: {0:.2f} seconds'.format(time.time() - process_time))
-    print('\nTotal Time: {0:.2f} seconds\n'.format(time.time() - start_time))
+    print('\nTotal Time: {0:.2f} seconds'.format(time.time() - start_time))
+    print('Total Processed Rows: {}'.format(total_counter_processed))
+    print('Total Written Records: {}'.format(total_counter_written))
+    print('Total Errors Found: {}\n'.format(total_error_found))
     logging.info('Total Execution Time: {0:.2f}'.format(time.time() - start_time))
+    logging.info('Total Processed Rows: {}'.format(total_counter_processed))
+    logging.info('Total Written Records: {}'.format(total_counter_written))
+    logging.info('Total Errors Found: {}'.format(total_error_found))
     logging.info('Data Processing Script Ends')
-
 
 if __name__ == '__main__':
     main()
